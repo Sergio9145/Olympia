56fe1ceec281762f5938ef294a63afa614fdfd15
Sergiy Opryshko <sopryshko9145@conestogac.on.ca>
2018-05-20 15:57:40 -0400

    First draft of the app

---
+++ bin/app/src/main/java/com/olympia/oxforddictionarysample/SampleApp.java
@@ -0,0 +1,20 @@
+package com.olympia.oxforddictionarysample;
+
+import android.app.Application;
+
+import com.olympia.oxfordapi.ApiClient;
+
+public class SampleApp extends Application {
+    private ApiClient apiClient;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        apiClient = new ApiClient();
+    }
+
+    public ApiClient apiClient() {
+        return apiClient;
+    }
+}
