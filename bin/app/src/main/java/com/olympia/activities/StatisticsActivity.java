670bdffeacb188e308c82e9256f45c307a759896
Sergiy Opryshko <sopryshko9145@conestogac.on.ca>
2018-07-10 22:50:17 -0400

    Added OK button to quiz statistics

--- bin/app/src/main/java/com/olympia/activities/StatisticsActivity.java
+++ bin/app/src/main/java/com/olympia/activities/StatisticsActivity.java
@@ -1,7 +1,10 @@
 package com.olympia.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.olympia.Globals;
@@ -20,5 +23,14 @@ public class StatisticsActivity extends AppCompatActivity {
 
         TextView t2 = findViewById(R.id.overall_score);
         t2.setText(String.valueOf(Quiz.overallScore));
+
+        Button b = findViewById(R.id.ok);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StatisticsActivity.this, WordsListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
