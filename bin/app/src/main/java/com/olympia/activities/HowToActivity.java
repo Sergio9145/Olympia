e8959002198c806087008eb4a1a628ab5fdea408
Sergiy Opryshko <sopryshko9145@conestogac.on.ca>
2018-07-24 11:17:26 -0400

    Improved HowTo manual

--- bin/app/src/main/java/com/olympia/activities/HowToActivity.java
+++ bin/app/src/main/java/com/olympia/activities/HowToActivity.java
@@ -27,22 +27,31 @@ public class HowToActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_howto);
 
-        ViewGroup transitionsContainer1 = findViewById(R.id.transitions_container1),
-                transitionsContainer2 = findViewById(R.id.transitions_container2),
-                transitionsContainer3 = findViewById(R.id.transitions_container3),
-                transitionsContainer4 = findViewById(R.id.transitions_container4),
-                transitionsContainer5 = findViewById(R.id.transitions_container5),
-                transitionsContainer6 = findViewById(R.id.transitions_container6);
+        ViewGroup transitionsContainer10 = findViewById(R.id.transitions_container10),
+                transitionsContainer20 = findViewById(R.id.transitions_container20),
+                transitionsContainer30 = findViewById(R.id.transitions_container30),
+                transitionsContainer40 = findViewById(R.id.transitions_container40),
+                transitionsContainer50 = findViewById(R.id.transitions_container50),
+                transitionsContainer60 = findViewById(R.id.transitions_container60),
+                transitionsContainer70 = findViewById(R.id.transitions_container70),
+                transitionsContainer80 = findViewById(R.id.transitions_container80),
+                transitionsContainer90 = findViewById(R.id.transitions_container90);
 
-        TextView line1 = transitionsContainer1.findViewById(R.id.line1),
-                line2 = transitionsContainer2.findViewById(R.id.line2),
-                line3 = transitionsContainer3.findViewById(R.id.line3),
-                line4 = transitionsContainer4.findViewById(R.id.line4),
-                line5 = transitionsContainer5.findViewById(R.id.line5);
-        ImageView img3 = transitionsContainer3.findViewById(R.id.img3),
-                img4 = transitionsContainer4.findViewById(R.id.img4),
-                img5 = transitionsContainer5.findViewById(R.id.img5);
-        Button btn = transitionsContainer6.findViewById(R.id.ok);
+        TextView line10 = transitionsContainer10.findViewById(R.id.line10),
+                line20 = transitionsContainer20.findViewById(R.id.line20),
+                line30 = transitionsContainer30.findViewById(R.id.line30),
+                line40 = transitionsContainer40.findViewById(R.id.line40),
+                line50 = transitionsContainer50.findViewById(R.id.line50),
+                line60 = transitionsContainer60.findViewById(R.id.line60),
+                line70 = transitionsContainer70.findViewById(R.id.line70),
+                line80 = transitionsContainer80.findViewById(R.id.line80);
+        ImageView img20 = transitionsContainer20.findViewById(R.id.img20),
+                img40 = transitionsContainer40.findViewById(R.id.img40),
+                img50 = transitionsContainer50.findViewById(R.id.img50),
+                img60 = transitionsContainer60.findViewById(R.id.img60),
+                img70 = transitionsContainer70.findViewById(R.id.img70),
+                img80 = transitionsContainer80.findViewById(R.id.img80);
+        Button btn = transitionsContainer90.findViewById(R.id.ok);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,21 +75,30 @@ public class HowToActivity extends AppCompatActivity {
                 Transition transition3 = new Slide(Gravity.START).setDuration(duration).setStartDelay(delay*2);
                 Transition transition4 = new Slide(Gravity.BOTTOM).setDuration(duration).setStartDelay(delay*3);
 
-                TransitionManager.beginDelayedTransition(transitionsContainer1, transition1);
-                TransitionManager.beginDelayedTransition(transitionsContainer2, transition2);
-                TransitionManager.beginDelayedTransition(transitionsContainer3, transition3);
-                TransitionManager.beginDelayedTransition(transitionsContainer4, transition4);
-                TransitionManager.beginDelayedTransition(transitionsContainer5, transition4);
-                TransitionManager.beginDelayedTransition(transitionsContainer6, transition1.setStartDelay(delay*4));
+                TransitionManager.beginDelayedTransition(transitionsContainer10, transition1);
+                TransitionManager.beginDelayedTransition(transitionsContainer20, transition2);
+                TransitionManager.beginDelayedTransition(transitionsContainer30, transition3);
+                TransitionManager.beginDelayedTransition(transitionsContainer40, transition4);
+                TransitionManager.beginDelayedTransition(transitionsContainer50, transition4);
+                TransitionManager.beginDelayedTransition(transitionsContainer60, transition4);
+                TransitionManager.beginDelayedTransition(transitionsContainer70, transition4);
+                TransitionManager.beginDelayedTransition(transitionsContainer80, transition4);
+                TransitionManager.beginDelayedTransition(transitionsContainer90, transition1.setStartDelay(delay*4));
 
-                line1.setVisibility(View.VISIBLE);
-                line2.setVisibility(View.VISIBLE);
-                line3.setVisibility(View.VISIBLE);
-                img3.setVisibility(View.VISIBLE);
-                line4.setVisibility(View.VISIBLE);
-                img4.setVisibility(View.VISIBLE);
-                line5.setVisibility(View.VISIBLE);
-                img5.setVisibility(View.VISIBLE);
+                line10.setVisibility(View.VISIBLE);
+                line20.setVisibility(View.VISIBLE);
+                img20.setVisibility(View.VISIBLE);
+                line30.setVisibility(View.VISIBLE);
+                line40.setVisibility(View.VISIBLE);
+                img40.setVisibility(View.VISIBLE);
+                line50.setVisibility(View.VISIBLE);
+                img50.setVisibility(View.VISIBLE);
+                line60.setVisibility(View.VISIBLE);
+                img60.setVisibility(View.VISIBLE);
+                line70.setVisibility(View.VISIBLE);
+                img70.setVisibility(View.VISIBLE);
+                line80.setVisibility(View.VISIBLE);
+                img80.setVisibility(View.VISIBLE);
                 btn.setVisibility(View.VISIBLE);
             }
         };
